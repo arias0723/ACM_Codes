c8402e3c5241d62b53b64ee02f6b52d25a21fd83
gH0ST <arias0723@gmail.com>
2018-04-22 18:55:31 -0400

    Adding new Trie DS (using vector base)

--- Data Structures/Trie/Trie.cpp
+++ Data Structures/Trie/Trie.cpp
@@ -1,143 +1,89 @@
-#include <bits/stdc++.h>
-#define endl '\n'
-using namespace std;
-typedef long long ll;
-typedef unsigned long long llu;
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef pair<int, int> pii;
-typedef vector<pii> vii;
+// Trie
+class Trie {
 
-// Add defines here ...
-const int MAXN = 100005;
-const int MOD = 2537;
-const int INF = 1e9;
+public:
+	Trie() {
+		addNode();
+	}
 
+	// Insert a new word in the Trie
+	void insert(char* str, const int id = 0) {
+		// Update node word count
+		nodes[id].word_cnt ++;
+		// End of word (... as we know it :D)
+		if(*str == 0) {
+			nodes[id].word_end ++;
+			return;
+		}
+		// Init
+		if(nodes[id].children[*str - first_char] == -1) {
+			int idx = addNode();
+			nodes[id].children[*str - first_char] = idx;
+		}
+		insert(str + 1, nodes[id].children[*str - first_char]);
+	}
+	// Removes an existing word (return true on success)
+	bool erase(char* str, const int id = 0) {
+		// Word not found
+		if(id == -1) return false;
+		// End of word 
+		if(*str == 0) {
+			// Exist in trie (can delete it)
+			if(nodes[id].word_end > 0) {
+				nodes[id].word_cnt --;
+				nodes[id].word_end --;
+				return true;
+			}
+			return false;
+		}
+		// Check if word exist
+		bool exist = erase(str + 1, nodes[id].children[*str - first_char]);
+		if(exist) {
+			nodes[id].word_cnt --;
+		}
+		return exist;
+	}
+	int find(char* str, const int id = 0) {
+		// Word not found
+		if(id == -1) return 0;
+		// End of word
+		if(*str == 0) {
+			return nodes[id].word_end;
+		}
+		return find(str + 1, nodes[id].children[*str - first_char]);
+	}
+	int countPrefix(char* prefix, const int id = 0) {
+		// Prefix not found
+		if(id == -1) return 0;
+		// End of word
+		if(*prefix == 0) {
+			return nodes[id].word_cnt;
+		}
+		return countPrefix(prefix + 1, nodes[id].children[*prefix - first_char]);
+	}
 
-// Input
-struct node {
-	int cnt;
-	node *left, *right; 
+private:
+	static const int alpha_size = 30;
+	static const char first_char = 'a';
+	struct Node {
+		// Node data
+		int id, word_cnt, word_end;
+		int children[alpha_size];
+
+		Node() {
+			id = word_cnt = word_end = 0;
+			memset(children, -1, sizeof children);
+		}
+	};
+	vector<Node> nodes;
+
+	// Push new node
+	int addNode() {
+		Node n;
+		n.id = nodes.size();
+		nodes.push_back(n);
+		return nodes.size() - 1;
+	}
 };
 
-int N, T, K, trieSz;
-node trie[50 * MAXN];
-
-node* addNode() {
-
-	trie[trieSz].cnt = 0;
-	trie[trieSz].left = 0;
-	trie[trieSz].right = 0;
-
-	return &(trie[trieSz ++]);
-}
-int calcCnt(node* r) { return (r == 0) ? 0 : r->cnt; }
-
-void insert(node* r, int x, int depth = 21) {
-
-	if(depth < 0) {
-		r->cnt ++;
-		return;
-	}
-
-	int mask = x&(1<<depth);
-	if(mask) {
-
-		if(r->right == 0) {
-			r->right = addNode();
-		}
-		insert(r->right, x, depth - 1);
-	}
-	else {
-
-		if(r->left == 0) {
-			r->left = addNode();
-		}
-		insert(r->left, x, depth - 1);
-	}
-
-	int v1 = calcCnt(r->left);
-	int v2 = calcCnt(r->right);	
-	r->cnt = v1 + v2;
-}
-
-int query(node* r, int x, int depth = 21) {
-
-	if(r == 0)
-		return 0;
-	if(depth < 0)
-		return 0;//return r->cnt;	
-
-	int mask = x&(1<<depth);
-	if(mask) {
-
-		if(K&(1<<depth)) {
-
-			return query(r->left, x, depth - 1) + calcCnt(r->right);
-		}
-		return query(r->right, x, depth - 1);
-	}
-	else {
-
-		if(K&(1<<depth)) {
-
-			return query(r->right, x, depth - 1) + calcCnt(r->left);
-		}
-		return query(r->left, x, depth - 1);
-	}
-}
-
-
-int main() {
-
-	//Input region
-	#ifdef GHOST
-	system("subl name.in");
-	freopen("name.in", "r", stdin);
-	FILE * FILE_NAME = freopen("name.out", "w", stdout);
-	int TIME = clock();
-	#endif
-
-
-	std::ios::sync_with_stdio(false); cin.tie(0);
-	//Add your code here...
-	
-
-
-	cin >> T;
-	while(T--) {
-
-		trieSz = 0;
-		node* root = addNode();
-
-		cin >> N >> K;
-
-		int x, cum = 0;
-		ll ans = 0;
-		// insert(trie, 0);
-		for (int i = 0; i < N; ++i)
-		{
-			cin >> x;
-			cum ^= x;			
-
-			ans += query(root, cum);
-			insert(root, cum);
-			//cout << ans << endl;
-		}
-		cout << ans << endl;
-	}
-
-
-
-
-	//Output region
-	#ifdef GHOST
-	cout << "\n\nTIME: " << (clock() - TIME) << " MS" << endl;
-	cout.flush();
-	fclose(FILE_NAME);
-	system("subl name.out");
-	#endif
-
-	return 0;
-}
-
+Trie T;
